aa8bf1925613528f0c3494e6032e9c1ceced4fab
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-10-31 22:52:47 +0530

    Update error.ts

--- exception/error.ts
+++ exception/error.ts
@@ -39,7 +39,727 @@ export class LongException {
     ) {
       const currentErrorMessage = errorOutputMessage[errorIndex];
       console.log(currentErrorMessage);
+    }import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
     }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+import {red, green} from 'chalk';
+
+export class LongException {
+  private readonly exceptionMessage: string;
+  private readonly suggestion: string;
+  private readonly type: string;
+
+  /**
+   * @constructor
+   *
+   * @param errorMessage The message to be thrown
+   * @param suggestion Any suggestions to fix the eror
+   * @param type The type of the errpr
+   */
+  constructor(errorMessage: string, suggestion: string, type: string) {
+    this.exceptionMessage = errorMessage.toString();
+    this.suggestion = suggestion;
+
+    this.type = type;
+  }
+
+  /**
+   * @private
+   *
+   * Throw out the exception message followed by
+   * the suggestion message
+   *
+   * @param exceptionType The type of the exception
+   */
+  public evokeLongException = (): void => {
+    const errorOutputMessage = [
+      red(`[${this.type}] ${this.exceptionMessage}`),
+      green(`[Suggestion] ${this.suggestion}`),
+    ];
+    for (
+      let errorIndex = 0;
+      errorIndex < errorOutputMessage.length;
+      errorIndex++
+    ) {
+      const currentErrorMessage = errorOutputMessage[errorIndex];
+      console.log(currentErrorMessage);
+    }
+    process.exit();
+  };
+}
+
     process.exit();
   };
 }
